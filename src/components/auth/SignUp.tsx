c7e026a02f5d668de23e474cee09827c53c97f1f
Ucandoit <yukan.zhao@gmail.com>
2019-04-12 21:10:48 +0200

    Feature: material ui for signup page Fix: yarn dependency problem + quote issue for windows command

--- src/components/auth/SignUp.tsx
+++ src/components/auth/SignUp.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
 import { withRouter } from 'react-router-dom';
-import { SignButton, SignForm } from './style';
+import { SignButton, SignForm, SignPaper, SignAvatar } from './style';
 import { FirebaseContext } from '../../firebase';
 import { RouteEnum } from '../../constants';
 import Input from '@material-ui/core/Input';
+import Typography from '@material-ui/core/Typography';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
 
 const SignUp: React.FunctionComponent<RouteComponentProps> = (props): JSX.Element => {
   const [username, setUserName] = React.useState<string>('');
@@ -19,46 +22,75 @@ const SignUp: React.FunctionComponent<RouteComponentProps> = (props): JSX.Elemen
   const { history } = props;
 
   return (
-    <SignForm
-      onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        firebase
-          .doCreateUserWithEmailAndPassword(email, password)
-          .then(auth => {
-            console.log(auth);
-            history.push(RouteEnum.DASHBOARD);
-          })
-          .catch(err => {
-            console.log(err);
-          });
-      }}
-    >
-      <Input
-        value={username}
-        onChange={e => setUserName(e.target.value)}
-        placeholder="请输入用户名"
-        type="text"
-        required
-      />
-      <Input value={email} onChange={e => setEmail(e.target.value)} placeholder="请输入邮箱" type="email" required />
-      <Input
-        value={password}
-        onChange={e => setPassword(e.target.value)}
-        placeholder="请输入密码"
-        type="password"
-        required
-      />
-      <Input
-        value={passwordCheck}
-        onChange={e => setPasswordCheck(e.target.value)}
-        placeholder="请确认密码"
-        type="password"
-        required
-      />
-      <SignButton disabled={isInvalid} type="submit">
+    <SignPaper>
+      <SignAvatar />
+      <Typography component="h1" variant="h5">
         注册
-      </SignButton>
-    </SignForm>
+      </Typography>
+      <SignForm
+        onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
+          e.preventDefault();
+          firebase
+            .doCreateUserWithEmailAndPassword(email, password)
+            .then(auth => {
+              console.log(auth);
+              history.push(RouteEnum.DASHBOARD);
+            })
+            .catch(err => {
+              console.log(err);
+            });
+        }}
+      >
+        <FormControl margin="normal" required fullWidth>
+          <InputLabel htmlFor="username">请输入用户名</InputLabel>
+          <Input
+            id="username"
+            name="username"
+            type="text"
+            autoComplete="请输入用户名"
+            value={username}
+            onChange={e => setUserName(e.target.value)}
+            autoFocus
+          />
+        </FormControl>
+        <FormControl margin="normal" required fullWidth>
+          <InputLabel htmlFor="email">请输入邮箱</InputLabel>
+          <Input
+            id="email"
+            name="email"
+            type="email"
+            autoComplete="请输入邮箱"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+        </FormControl>
+        <FormControl margin="normal" required fullWidth>
+          <InputLabel htmlFor="password">请输入密码</InputLabel>
+          <Input
+            id="password"
+            name="password"
+            type="password"
+            autoComplete="请输入密码"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+        </FormControl>
+        <FormControl margin="normal" required fullWidth>
+          <InputLabel htmlFor="passwordCheck">请确认密码</InputLabel>
+          <Input
+            id="passwordCheck"
+            name="passwordCheck"
+            type="password"
+            autoComplete="请确认密码"
+            value={passwordCheck}
+            onChange={e => setPasswordCheck(e.target.value)}
+          />
+        </FormControl>
+        <SignButton type="submit" fullWidth variant="contained" color="primary" disabled={isInvalid}>
+          注册
+        </SignButton>
+      </SignForm>
+    </SignPaper>
   );
 };
 
